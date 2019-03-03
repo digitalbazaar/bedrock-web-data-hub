cf99952056902311177de8537d4b5b54096095b7
Dave Longley <dlongley@digitalbazaar.com>
2019-03-02 20:15:04 -0500

    Fix basic tests.

--- test/web/mock.js
+++ test/web/mock.js
@@ -9,31 +9,7 @@ import {AccountMasterKey, KmsService} from 'bedrock-web-kms';
 
 export const mock = {};
 
-// const staticMasterKey = {
-//   unprotected: {
-//     alg: 'PBES2-HS512+A256KW',
-//     p2c: 4096,
-//     p2s: 'd7l6Ub5T0eZlpWjhSGI3Q19DtcogEkHg1hN8JzORj4U'
-//   },
-//   encrypted_key:
-//     'HrLOox-iCFlwCsQIWAWJ7UCuzjt2jdzOv92rEFNYymNX0XiIE_k8U-' +
-//     'z_Y3kCc_xqQ_wob904Q3XJxwzsO6xla7plr54MVh0N'
-// };
-
-// const staticDoc = {
-//   id: 'lcm5RDZGuDmxlFZSc0k468LcfiY0viSxm7UIBFPKKVk',
-//   attributes: [],
-//   jwe: {
-//     unprotected: {
-//       alg: 'A256KW',
-//       enc: 'A256GCM'
-//     },
-//     encrypted_key: 'BZHh3ExmG56fJtGbb3L_tfJe9WNQHaDK1XmmO807pjoFuuDktdqfUQ',
-//     iv: 'PcpoGFRyHYBuPIu1',
-//     ciphertext: 'RR1VJiV16uxzjbyaprspvAuuso2J2AnB8GgbQvu07D56IA',
-//     tag: 'SHj-mrvbuLWLLTWW8EQ_8w'
-//   }
-// };
+// TODO: add some static data to test against
 
 mock.init = async () => {
   // create mock server
@@ -83,14 +59,16 @@ mock.init = async () => {
   }
 };
 
-mock.createDataHub = async () => {
+mock.createDataHub = async ({primary = false} = {}) => {
   const dhs = new DataHubService();
   const {kek, hmac} = mock.keys;
-  const config = await dhs.create({
-    config: {
-      kek: {id: kek.id, algorithm: kek.algorithm},
-      hmac: {id: hmac.id, algorithm: hmac.algorithm}
-    }
-  });
+  let config = {
+    kek: {id: kek.id, algorithm: kek.algorithm},
+    hmac: {id: hmac.id, algorithm: hmac.algorithm}
+  };
+  if(primary) {
+    config.primary = true;
+  }
+  config = await dhs.create({config});
   return new DataHub({config, kek, hmac});
 };
